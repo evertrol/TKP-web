beea205c89199c14fcaec746238ee3c84076c830
Evert Rol <evert.astro@gmail.com>
2012-02-22 15:18:03 +0100

    Allow for multiple database servers.

--- tkpweb/apps/database/urls.py
+++ tkpweb/apps/database/urls.py
@@ -4,6 +4,8 @@ from .views import DataBaseView
 
 urlpatterns = patterns(
    '',
+   url(r'^(?P<host>\w+):(?P<port>\d+)/(?P<database>\w+)/$', view=DataBaseView.as_view(), name='database-port'),
+   url(r'^(?P<host>\w+)/(?P<database>\w+)/$', view=DataBaseView.as_view(), name='database-host'),
    url(r'^(?P<database>\w+)/$', view=DataBaseView.as_view(), name='database'),
    url(r'^$', view=DataBaseView.as_view(), name='index')
    )
